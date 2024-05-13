48befa134d340cde863c202889ff29481bd4da7b
ariezieq <160419356+ariezieq@users.noreply.github.com>
2024-05-13 09:49:18 +0700

    Add files via upload

---
+++ tugas1.py
@@ -0,0 +1,16 @@
+angka = []
+angka.insert (0,6)
+angka.insert (1,5)
+angka.insert (2,10)
+print (angka)
+angka.remove (6)
+angka.append (1)
+angka.append (9)
+angka.sort ()
+print (angka)
+angka.pop (-1)
+angka.reverse ()
+print (angka)
+
+
+
